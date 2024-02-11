99c3efc02faf966c629bbf427ec7a69f3e5cdaac
Adam Baranczyk <baranczykadam@gmail.com>
2024-02-11 15:28:01 +0100

    added menu

--- Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/task/src/cinema/Cinema.java
@@ -1,9 +1,59 @@
 package cinema;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Cinema {
+    public static void menu(Scanner scanner, int rows, int seatsInRow, boolean [][] seatsMatrix){
+        System.out.println("1. Show the seats");
+        System.out.println("2. Buy a ticket");
+        System.out.println("0. Exit");
+        int choice = scanner.nextInt();
+        if(choice == 1){
+            System.out.println("Cinema:");
+            System.out.print(" ");
+            for(int i=1; i<=seatsInRow; i++){
+                System.out.print(" " + i);
+            }
+            System.out.println();
+            for(int i=0; i<rows; i++){
+                System.out.print(i+1);
+                for(int j=0; j<seatsInRow; j++){
+                    if(seatsMatrix[i][j] == false){
+                        System.out.print(" S");
+                    }else{
+                        System.out.print(" B");
+                    }
+                }
+                System.out.println();
+            }
+            menu(scanner,rows,seatsInRow, seatsMatrix);
+        } else if (choice == 2) {
+            int [] seat = new int[2];
+            System.out.println("Enter a row number:");
+            System.out.println("> ");
+            seat[0] = scanner.nextInt();
+            System.out.println("Enter a seat number in that row:");
+            System.out.println("> ");
+            seat[1] = scanner.nextInt();
+            seatsMatrix[seat[0]-1][seat[1]-1] = true;
+            System.out.print("Ticket price:");
+            int ticketPrice = 0;
+            if(rows * seatsInRow <= 60){
+                ticketPrice = 10;
+            }else {
+                if (seat[0] <= rows / 2) {
+                    ticketPrice = 10;
+                } else {
+                    ticketPrice = 8;
+                }
+            }
+            System.out.println(" $" + ticketPrice);
+            menu(scanner,rows,seatsInRow, seatsMatrix);
+        } else if (choice == 0) {
 
+        }
+    }
     public static void main(String[] args) {
         // Write your code here
         Scanner scanner = new Scanner(System.in);
@@ -13,63 +63,14 @@ public class Cinema {
         System.out.println("> ");
         System.out.println("Enter the number of seats in each row");
         int seatsInRow = scanner.nextInt();
-        System.out.println("Cinema:");
-        System.out.print(" ");
-        for(int i=1; i<=seatsInRow; i++){
-            System.out.print(" " + i);
+        boolean [][] seatsMatrix = new boolean[rows][seatsInRow];
+        for (boolean[] matrix : seatsMatrix) {
+            Arrays.fill(matrix, false);
         }
-        System.out.println();
-        for(int i=1; i<=rows; i++){
-            System.out.print(i);
-            for(int j=1; j<=seatsInRow; j++){
-                System.out.print(" S");
-            }
-            System.out.println();
-        }
+        //menu
+        menu(scanner, rows, seatsInRow, seatsMatrix);
 
-        System.out.println("Total income:");
-        if(rows * seatsInRow <= 60){
-            System.out.println("$" + rows * seatsInRow * 10);
-        }else{
-            int income = 0;
-            income = seatsInRow * (rows/2) *10 + seatsInRow *(rows-rows/2)*8;
-            System.out.println("$" + income);
-        }
-        int [] seat = new int[2];
-        System.out.println("Enter a row number:");
-        System.out.println("> ");
-        seat[0] = scanner.nextInt();
-        System.out.println("Enter a seat number in that row:");
-        System.out.println("> ");
-        seat[1] = scanner.nextInt();
-        System.out.print("Ticket price:");
-        int ticketPrice = 0;
-        if(rows * seatsInRow <= 60){
-            ticketPrice = 10;
-        }else {
-            if (seat[0] <= rows / 2) {
-                ticketPrice = 10;
-            } else {
-                ticketPrice = 8;
-            }
-        }
-        System.out.println(" $" + ticketPrice);
-        System.out.println("Cinema:");
-        System.out.print(" ");
-        for(int i=1; i<=seatsInRow; i++){
-            System.out.print(" " + i);
-        }
-        System.out.println();
-        for(int i=1; i<=rows; i++ ){
-            System.out.print(i);
-            for(int j=1; j<=seatsInRow; j++){
-                if(i==seat[0] && j==seat[1]){
-                    System.out.print(" B");
-                    continue;
-                }
-                System.out.print(" S");
-            }
-            System.out.println();
-        }
+
+
     }
 }
